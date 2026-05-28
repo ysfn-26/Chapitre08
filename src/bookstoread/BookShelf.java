b5cf879961dddc0e176847fe3c727d355d9d59c2
Mamady <madess10chiffres45@gmail.com>
2026-05-28 10:25:52 +0000

    premier commit

---
+++ src/bookstoread/BookShelf.java
@@ -0,0 +1,9 @@
+package bookstoread;
+import java.util.Collections;
+import java.util.List;
+    public class BookShelf {
+        public List<String> books() {
+            return Collections.emptyList();
+        }
+    }
+
