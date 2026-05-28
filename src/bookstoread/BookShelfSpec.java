4e9afa18a47ae517f67fd14cf6ccd40893fe4e8a
Mamady <madess10chiffres45@gmail.com>
2026-05-28 11:05:19 +0000

    methode Add() a été refactorisé

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,5 +1,6 @@
 package bookstoread;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -12,5 +13,13 @@ public class BookShelfSpec {
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java");
+        shelf.add("Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
     }
 }
