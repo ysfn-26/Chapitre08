a7b270598c4351934c49faa60b213fb34a97d731
noella <noellayrogbo1@gmail.com>
2026-06-08 11:15:32 +0000

    ajout de la vérification du regroupement par auteur avec assertj

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -2,6 +2,7 @@ package bookstoread;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.Collections;
@@ -77,5 +78,39 @@ public class BookShelfSpec {
         assertThat(booksByAuthor).containsKey("Robert C. Martin").containsValues(Collections.singletonList(cleanCode));
     }
 
+    @Nested
+    @DisplayName("Est vide")
+    class IsEmpty {
+        @Test
+        @DisplayName("Quand aucun livre n'y est ajouté")
+        public void emptyBookShelfWhenNoBookAdded() {
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
 
+        @Test
+        @DisplayName("Quand add est appelé sans livres")
+        void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+            shelf.add();
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
+    }
+
+    @Nested
+    @DisplayName("Après avoir ajouté des livres")
+    class BooksAreAdded {
+        @Test
+        @DisplayName("Contient deux livres")
+        void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+            // Test case removed for brevity
+        }
+
+        @Test
+        @DisplayName("Renvoie au client une collection de livres immuable ")
+        void bookshelfIsImmutableForClient() {
+            // Test case removed for brevity
+        }
+
+    }
 }
